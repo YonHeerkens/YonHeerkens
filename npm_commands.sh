d4bcacc5bc7555cedc8922882ba2b5ebbc4d8e06
Yon <Yon.heerkens@gmail.com>
2025-06-24 17:11:11 +0200

    add user fetch from database

---
+++ npm_commands.sh
@@ -0,0 +1,11 @@
+npm install package-name     # Add new package
+npm uninstall package-name   # Remove package
+npm update                   # Update all packages to newer versions
+npm list                     # See what's installed
+
+npm start                    # Usually starts your development server
+npm test                     # Runs your tests
+npm run build               # Usually builds your project for production
+
+npm info package-name        # See info about a package
+npm outdated                 # See which packages have newer versions
